04f38369d3dac7db2649bca132b6f350f0115edb
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-07 00:43:34 +0800

    Disable OPOST

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@ void EnableRawModel()
     atexit(DisableRawModel);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
