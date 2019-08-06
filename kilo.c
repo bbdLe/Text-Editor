16bc5ad166c23d6bbc1179c50b9125028a59d471
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-06 23:07:39 +0800

    turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -1,12 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void DisableRawModel()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void EnableRawModel()
 {
-    struct termios raw;
-
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(DisableRawModel);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -14,6 +24,16 @@ int main()
 {
     EnableRawModel();
     char c;
-    while (read(STDIN_FILENO, &c, 1) && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) && c != 'q')
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
