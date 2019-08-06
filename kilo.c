2cde240b9423a964ef1a0b37e1a253b2991d3cf9
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-07 00:15:15 +0800

    Disable Ctrl-S and Ctrl-Q

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@ void EnableRawModel()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(DisableRawModel);
     struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
