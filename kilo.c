90e24a7d9e5215d83b4af05464180fcd0086b0d3
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-07 00:22:05 +0800

    Disable Ctrl-V

--- kilo.c
+++ kilo.c
@@ -17,7 +17,7 @@ void EnableRawModel()
     atexit(DisableRawModel);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
