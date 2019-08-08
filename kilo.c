c82239948498fc89676ae2348e614f94446d6718
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-09 01:27:36 +0800

    Add tildes

--- kilo.c
+++ kilo.c
@@ -73,9 +73,22 @@ void EditorProcessKey()
     }
 }
 
+void EditorDrawRows()
+{
+    int y;
+
+    for (y = 0; y < 24; ++y)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void EditorRefreshScreen()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    EditorDrawRows();
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
