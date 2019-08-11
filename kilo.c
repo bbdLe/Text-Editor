a603a244896ce7f9cde18d8de0d598533bcf6adf
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-11 22:38:25 +0800

    detect page down and page up

--- kilo.c
+++ kilo.c
@@ -17,6 +17,8 @@ enum EditorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
 };
 
 struct EditorConfig
@@ -117,16 +119,37 @@ int EditorReadKey()
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] > '0' && seq[1] <= '9')
             {
-                case 'A':
-                    return ARROW_UP;
-                case 'B':
-                    return ARROW_DOWN;
-                case 'C':
-                    return ARROW_RIGHT;
-                case 'D':
-                    return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                {
+                    return '\x1b';
+                }
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                        case '5':
+                            return PAGE_UP;
+                        case '6':
+                            return PAGE_DOWN;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                    case 'A':
+                        return ARROW_UP;
+                    case 'B':
+                        return ARROW_DOWN;
+                    case 'C':
+                        return ARROW_RIGHT;
+                    case 'D':
+                 
+                       return ARROW_LEFT;
+                }
             }
         }
 
