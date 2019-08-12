ca238fa5fbebdbac1241dcb5c512f5e4b4dda97e
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-12 13:43:33 +0800

    Fix Tab Cursor

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ struct EditorConfig
 {
     int cx;
     int cy;
+    int rx;
     int rowoff;
     int coloff;
     int screenrows;
@@ -351,8 +352,30 @@ void EditorDrawRows(struct ABuf* aBuf)
     }
 }
 
+int EditorRowCxToRx(ERow* row, int cx)
+{
+    int rx = 0;
+    
+    for (int j = 0; j < cx; ++j)
+    {
+        if (row->chars[j] == '\t')
+        {
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        }
+        ++rx;
+    }
+
+    return rx;
+}
+
 void EditorScroll()
 {
+    E.rx = 0;
+    if (E.cy < E.numrows)
+    {
+        E.rx = EditorRowCxToRx(&E.row[E.cy], E.cx);
+    }
+
     if (E.cy < E.rowoff)
     {
         E.rowoff = E.cy;
@@ -363,14 +386,14 @@ void EditorScroll()
         E.rowoff = E.cy - E.screenrows + 1;
     }
 
-    if (E.cx < E.coloff)
+    if (E.rx < E.coloff)
     {   
-        E.coloff = E.cx;
+        E.coloff = E.rx;
     }
 
-    if (E.cx >= E.coloff + E.screencols)
+    if (E.rx >= E.coloff + E.screencols)
     {
-        E.coloff = E.cx - E.screencols + 1;
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -386,7 +409,7 @@ void EditorRefreshScreen()
     EditorDrawRows(&aBuf);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy - E.rowoff + 1, E.cx - E.coloff + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy - E.rowoff + 1, E.rx - E.coloff + 1);
     AbAppend(&aBuf, buf, strlen(buf));
 
     AbAppend(&aBuf, "\x1b[?25h", 6);
@@ -533,6 +556,7 @@ void InitEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
