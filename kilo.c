484de2850f209aa55a38bb8b3f0b475778afd36a
bbdLe <dalibaxiaoliba19@gmail.com>
2019-08-13 17:54:13 +0800

    search arrow

--- kilo.c
+++ kilo.c
@@ -165,18 +165,52 @@ char* EditorRowsToString(int* bufLen)
 
 void EditorFindCallback(char* query, int key)
 {
+    static int last_match = -1;
+    static int direction = 1;
+
     if (key == '\r' || key == '\x1b')
     {
+        last_match = -1;
+        direction = 1;
         return;
     }
+    else if (key == ARROW_RIGHT || key == ARROW_DOWN)
+    {
+        direction = 1;
+    }
+    else if (key == ARROW_LEFT || key == ARROW_UP)
+    {
+        direction = -1;
+    }
+    else
+    {
+        last_match = -1;
+    }
 
+    if (last_match == -1)
+    {
+        direction = 1;
+    }
+    
+    int current = last_match;
     for (int i = 0; i < E.numrows; ++i)
     {
-        ERow* row = &E.row[i];
+        current += direction;
+        if (current == -1)
+        {
+            current = E.numrows - 1;
+        }
+        else if (current == E.numrows)
+        {
+            current = 0; 
+        }
+
+        ERow* row = &E.row[current];
         char* match = strstr(row->render, query);
         if (match)
         {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = EditorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
